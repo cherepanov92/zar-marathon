cb39755468e0ddccdb6803a749dcf741f7ec4f86
cherepanov92 <cherepanov92@gmail.com>
2021-01-28 22:35:51 +0500

    [*] Компонент Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './style.module.css';
+
+const Header = ({ title, descr }) => {
+  return (
+    <header className={`${style.root}`}>
+      <div className={`${style.forest}`} />
+      <div className={`${style.container}`}>
+        <h1>{title}</h1>
+        <p>{descr}</p>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
