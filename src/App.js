4c1ed3ff2be015e80f2936c9441e83bacba74de1
cherepanov92 <cherepanov92@gmail.com>
2021-01-31 16:20:33 +0500

    [*] Рендеринг покемонов с данными из "БД"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import PokemonDB from './db';
+import PokemonCard from './components/PokemonCard/PokemonCard';
 
 import Header from './components/Header/Header';
 import Layout from './components/Layout/Layout';
@@ -12,9 +14,29 @@ function App() {
   return (
     <>
       <Header title='title' descr='descr'/>
-      <Layout id='1' title='title 1' descr='deskr 1' urlBg={Bg1}/>
-      <Layout id='2' title='title 2' descr='deskr 2' colorBg='red'/>
-      <Layout id='3' title='title 3' descr='deskr 3' urlBg={Bg2}/>
+      {/* <Layout 
+        id='1' 
+        title='title 1' 
+        urlBg={Bg1}
+      >
+      </Layout> */}
+      <Layout id='2' title='Мои карточки' colorBg='red'>
+        <div className='flex'>
+          {PokemonDB.map(pokemon => 
+            <PokemonCard 
+              key={pokemon.id} 
+              id={pokemon.id}
+              name={pokemon.name}
+              type={pokemon.type}
+              img={pokemon.img}
+              values={pokemon.values} 
+            />)
+          }
+        </div>
+      </Layout>
+      {/* <Layout id='3' title='title 3' urlBg={Bg2}>
+        <h1>test 3</h1>
+      </Layout> */}
       <Footer />
     </>
   );
