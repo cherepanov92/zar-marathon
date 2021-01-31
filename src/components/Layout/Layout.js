60c701b95c94c65c0b780a30aeac34e099ff26fe
cherepanov92 <cherepanov92@gmail.com>
2021-01-31 22:59:14 +0500

    [fix] поправил стили слоёв

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,21 +3,24 @@ import style from './style.module.css';
 
 const Layout = ({id, title, urlBg, colorBg, children}) => {
   return (
-    <section className={`${style.root}`} id={ id }>
-      <div 
-        className={`${style.wrapper}`} 
-        style={{
+    <section 
+      className={`${style.root}`} 
+      id={ id }
+      style={{
           backgroundImage: urlBg ? `url(${urlBg})`: null,
           backgroundColor: colorBg ? colorBg: null,
           backgroundRepeat: "repeat-x",
           backgroundPosition: "bottom",
-        }}
+      }}
+    >
+      <div 
+        className={`${style.wrapper}`} 
       >
         <div className={`${style.title}`}>
           <h3>{ title }</h3>
           <span className={`${style.separator}`}></span>
         </div>
-        <div className={`${style.descr} ${style.full}`}>
+        <div className={`${style.desc} ${style.full}`}>
           {children}
         </div>
       </div>
