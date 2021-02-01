ded3297ef92bce378dd22b145c1a4c91ef8b8857
cherepanov92 <cherepanov92@gmail.com>
2021-02-01 22:53:46 +0500

    [*] Смена роута на страницу Game

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import style from './style.module.css';
 
-const Header = ({ title, descr }) => {
+const Header = ({ title, onClickButton, children }) => {
+
+  const handleClick = () => {
+    onClickButton && onClickButton('game');
+  }
+
   return (
     <header className={`${style.root}`}>
       <div className={`${style.forest}`} />
       <div className={`${style.container}`}>
         <h1>{title}</h1>
-        <p>{descr}</p>
+        {children}
+        <button onClick={handleClick}>
+            Start game
+        </button>
       </div>
     </header>
   )
