7bb2aa052197396c94a549eed57ebea89bb771f9
cherepanov92 <cherepanov92@gmail.com>
2021-02-04 00:41:45 +0500

    [*] Перевёл App на react-router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from './routes/Home';
 import GamePage from './routes/Game';
 
 const App = () => {
-  const [page, setPage] = useState('home');
-  const handleChangePage = (page) => {
-    setPage(page);
-  }
-
-  switch (page) {
-    case 'home':
-      return <HomePage onChangePage={handleChangePage} />
-    case 'game':
-      return <GamePage onChangePage={handleChangePage} />
-    default:
-      return <HomePage />
-  }
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/home" component={HomePage} />
+        <Route path="/game" component={GamePage} />
+        <Route path="/about" render={() => (
+          <h1> This is page about! </h1>
+        )} />
+        <Route render={() => (
+          <h1> 404 page not found! </h1>
+        )} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
