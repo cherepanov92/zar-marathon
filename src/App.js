0a5375170ada106bb879c04547baa04788333b59
cherepanov92 <cherepanov92@gmail.com>
2021-02-04 18:19:51 +0500

    [*] Переписал роуты с использованием редиректа

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch, Redirect } from 'react-router-dom';
 import cn from 'classnames';
 
 import HomePage from './routes/Home';
@@ -9,29 +9,36 @@ import Footer from './components/Footer/Footer';
 import style from './style.module.css';
 
 const App = () => {
-  const match = useRouteMatch('/');
+  const match = useRouteMatch('/home');
 
   return (
     <Switch>
+      <Route path="/404" render={() => (
+        <h1> 404 page not found! </h1>
+      )} />
       <Route>
         <>
-          <MenuHeader bgActive={!match.isExact}/>
-          <div className={cn(style.wrap, {[style.isHomePage]: match.isExact})}>
+          <MenuHeader bgActive={!match || !match.isExact}/>
+          <div className={cn(style.wrap, {[style.isHomePage]: match && match.isExact})}>
             <Switch>
-              <Route path="/" exact component={HomePage} />
+              <Route 
+                path="/" 
+                exact 
+                render={() => (<Redirect to="/home" />)} 
+              />
               <Route path="/home" component={HomePage} />
               <Route path="/game" component={GamePage} />
               <Route path="/about" render={() => (
                 <h1> This is page about! </h1>
+              )} />
+              <Route render={() => (
+                <Redirect to="/404" />
               )} />
             </Switch>
           </div>
           <Footer/>
         </>
       </Route>
-      <Route render={() => (
-        <h1> 404 page not found! </h1>
-      )} />
     </Switch>
   )
 }
