18adfc29d5d93c7d209bb23deb65dffdeafb0db3
cherepanov92 <cherepanov92@gmail.com>
2021-02-07 23:06:51 +0500

    [*] Подключил Firebase DB

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch, useRouteMatch, Redirect } from 'react-router-dom';
 import cn from 'classnames';
+import firebase from 'firebase';
 
 import MenuHeader from './components/MenuHeader';
 import Footer from './components/Footer/Footer';
@@ -11,6 +12,19 @@ import HomePage from './routes/Home';
 import NotFoundPage from './routes/NotFound';
 
 import style from './style.module.css';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAje9dkqyMdyaOfN0WM-Y7WCh-nNN6iUgU",
+  authDomain: "pokemon-game-f2ef8.firebaseapp.com",
+  databaseURL: "https://pokemon-game-f2ef8-default-rtdb.firebaseio.com",
+  projectId: "pokemon-game-f2ef8",
+  storageBucket: "pokemon-game-f2ef8.appspot.com",
+  messagingSenderId: "636593100078",
+  appId: "1:636593100078:web:36e9eb132b5fb5fd2fd6bf"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
 const App = () => {
   const match = useRouteMatch('/home');
