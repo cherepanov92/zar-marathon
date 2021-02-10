e637559cf63d219f56970599761198e9e9695839
cherepanov92 <cherepanov92@gmail.com>
2021-02-10 11:34:31 +0500

    [*] Подключил PokemonContext к Game

--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 import StartPage from './routes/Start';
 import BoardPage from './routes/Board';
 import FinishPage from './routes/Finish';
 
+import { PokemonContext } from '../../context/pokemonContext';
+
 const GamePage = () => {
   const match = useRouteMatch();
+  const [pokemon, setPokemon] = useState([]);
+
   return (
+    <PokemonContext.Provider value={{pokemon, setPokemon}}>
       <Switch>
-          <Route path={`${match.path}/`} exact component={StartPage} />
-          <Route path={`${match.path}/board`} component={BoardPage} />
-          <Route path={`${match.path}/finish`} component={FinishPage} />
+        <Route path={`${match.path}/`} exact component={StartPage} />
+        <Route path={`${match.path}/board`} component={BoardPage} />
+        <Route path={`${match.path}/finish`} component={FinishPage} />
       </Switch>
+    </PokemonContext.Provider>
   );
 };
 
