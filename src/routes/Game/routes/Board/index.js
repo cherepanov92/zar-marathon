99ce031372717fd0e095353532c54d8c88bd643b
cherepanov92 <cherepanov92@gmail.com>
2021-02-14 21:09:17 +0500

    [*] Релизовал отображение карточек второго игрока

--- src/routes/Game/routes/Board/index.js
+++ src/routes/Game/routes/Board/index.js
@@ -9,17 +9,22 @@ import s from './style.module.css';
 const BoardPage = () => {
     const history = useHistory();
     const {pokemons} = useContext(PokemonContext);
-    const [board, setBoard] = useState([])
+    const [board, setBoard] = useState([]);
+    const [playerTwoState, setPlayerTwoState] = useState([]);
 
     if (!Object.keys(pokemons).length) {
         history.replace('/game');
     }
 
     useEffect(async () => {
-        const boardResponse = await fetch('https://reactmarathon-api.netlify.app/api/board');
-        const boardRequest = await boardResponse.json();
+        const boardRequest = await fetch('https://reactmarathon-api.netlify.app/api/board');
+        const boardResponse = await boardRequest.json();
+        setBoard(boardResponse.data);
 
-        setBoard(boardRequest.data);
+        const playerTwoRequest = await fetch('https://reactmarathon-api.netlify.app/api/create-player');
+        const playerTwoResponse = await playerTwoRequest.json();
+        setPlayerTwoState(playerTwoResponse.data);
+
     },[])
 
     const handlerClickBoardPlate = (position) => {
@@ -41,6 +46,22 @@ const BoardPage = () => {
                                 minimize
                                 className={s.card}
                             />)
+                        }
+						</div>
+                        <div className={s.playerTwo}>
+                        {
+                            playerTwoState.map(({name, img, id, type, values}) =>
+                                <PokemonCard 
+                                    key={id} 
+                                    id={id}
+                                    name={name}
+                                    type={type}
+                                    img={img}
+                                    values={values} 
+                                    minimize
+                                    className={s.card}
+                                />
+                            )
                         }
 						</div>
             <div className={s.board}>
