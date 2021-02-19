f1ef9902eb60c7099795d781a95f5f26c3c21ea9
cherepanov92 <cherepanov92@gmail.com>
2021-02-19 13:05:04 +0500

    [*] Базовое использование redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// import store from 'react-redux';
 
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+
+import rootReducer, {plusAction} from './store/counter';
+
+const store = new createStore(rootReducer);
+console.log('store', store)
+console.log('store getState', store.getState())
+store.subscribe(() => console.log('subscribe',store.getState()))
+
+store.dispatch(plusAction(10));
+
 
 ReactDOM.render(
   <BrowserRouter>
